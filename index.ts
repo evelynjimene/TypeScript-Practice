6cf04d2d403f6718149e6437ba7e5e38bf344850
Evelyn Jimenez <159857814+evelynjimene@users.noreply.github.com>
2024-03-14 20:55:34 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+let phoneNumber : string; // add type declaration to inform TypeScript that it should be a string
+
+if (Math.random() > 0.5) {
+  phoneNumber = '+61770102062';
+} else {
+  phoneNumber = '7167762323'; 
+}
